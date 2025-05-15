ee3093e4545b4ab7c49b002ce284c7af79fdd591
Lucas Superina <105914669@student.swin.edu.au>
2025-05-15 17:44:37 +1000

    Previous commit didnt have the changes in it oopps

---
+++ project2/index.php
@@ -0,0 +1,29 @@
+<!--JAY KSHIRSAGAR 105912265-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="description" content="A home page for Terrible Software Inc., containing information about the company">
+    <meta name="keywords" content="About, About Page, Innovation, Impact, Terrible Software Inc.">
+
+    <link rel="stylesheet" href="./styles/styles.css">
+    <title>Terrible Software Inc - Home</title>
+</head>
+
+<body class="home">
+    <div class="container">
+        <div class="glass-container">
+        <?php
+            include "header.inc";
+            createHeader("Home");
+        ?>
+        
+        <p>Imagine the rest of the page is here.</p>
+
+        </div>
+    </div>
+</body>
+<?php include "footer.inc"?>
