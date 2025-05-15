5f344ae57bbeef6d00c0b29df6889f6de0255d57
Lucas Superina <105914669@student.swin.edu.au>
2025-05-15 23:51:17 +1000

    Created Jobs page, has reusable PHP funtionality with static variables, as SQL database has not been set up yet.

---
+++ project2/jobs.php
@@ -0,0 +1,41 @@
+<!--PUJAN KUKADIYA 105920242-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="description" content="A job listing page for Terrible Software Inc., which lists all the available positions the company is hiring">
+    <meta name="keywords" content="Jobs, Hiring, Position, Machine Learning, Software Developer">
+
+    <link rel="stylesheet" href="./styles/styles.css">
+    <title>Terrible Software Inc - Jobs</title>
+</head>
+
+<?php
+    include "job_listing.inc";
+
+    include "header.inc";
+?>
+
+<body class="jobs">
+    <div class="container">
+        <div class="glass-container">
+            <?php createHeader("About"); ?>
+
+            <div class="content">
+                <div class="main">
+                    <h1 id="job_listing_header">Job Listings</h1>
+                    <?php
+                        job_listing();
+                        job_listing();
+                    ?>    
+                </div>
+            </div>
+
+        </div>
+    </div>
+</body>
+<?php include "footer.inc"?>
+</html>
