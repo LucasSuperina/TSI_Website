7d222687ed9ef761dd1be82953179150f4440f3a
Maggie-XinYiLaw <103488683@student.swin.edu.au>
2025-05-20 21:20:09 +1000

    Missing ";" in process_eoi.php

--- project2/process_eoi.php
+++ project2/process_eoi.php
@@ -1,13 +1,13 @@
 <?php
-    $reference_number = $_POST["job-reference-number"]
-    $first_name = $_POST["first-name"]
-    $last_name = $_POST["last-name"]
-    $dob = $_POST["dob"]
-    $gender = $_POST["gender"]
-    $street_address = $_POST["street-address"]
-    $suburb = $_POST["suburb"]
-    $state = $_POST["state"]
-    $postcode = $_POST["postcode"]
-    $email = $_POST["email"]
-    $phone = $_POST["phone"]
+    $reference_number = $_POST["job-reference-number"];
+    $first_name = $_POST["first-name"];
+    $last_name = $_POST["last-name"];
+    $dob = $_POST["dob"];
+    $gender = $_POST["gender"];
+    $street_address = $_POST["street-address"];
+    $suburb = $_POST["suburb"];
+    $state = $_POST["state"];
+    $postcode = $_POST["postcode"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
 ?>
