60f09935e769ada1c0cd410a49f4c99c3beb9a0d
Lucas Superina <105914669@student.swin.edu.au>
2025-05-26 23:10:24 +1000

    aaaaa

---
+++ project2/enhancements.php
@@ -0,0 +1,10 @@
+Provide the manager with the ability to select the field on which to sort the order in which the EOI records are displayed.
+
+Create a manager registration page with server side validation requiring unique username and a password rule, and store this information in a table.
+
+Control access to manage.php by checking username and password.
+
+Have access to the web site disabled for user a period of time on, say, three or more invalid login attempts.
+
+
+All of these have been done. The final enhancement applies for both "applicant" users, and "manager" users.
